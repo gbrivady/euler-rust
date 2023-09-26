ed870fa4e779ef5d47534f3f1f702e4cdcf5c47a
gbrivady <12777915+gbrivady@users.noreply.github.com>
2023-09-26 18:54:10 +0200

    Add 01

--- src/bin/0001.rs
+++ src/bin/0001.rs
@@ -1,3 +1,12 @@
 fn main() {
     let mut sum: u64 = 0;
+    for i in 0..334 {
+        sum += 3 * i;
+    }
+    for j in 0..200 {
+        if j % 3 != 0 {
+            sum += 5 * j;
+        }
+    }
+    print!("{}", sum);
 }
